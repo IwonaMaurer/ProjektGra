32396f3a65cd6184b5245c9b23a538691022dac1
IwonaMaurer <plastimex2@wp.pl>
2017-11-06 10:47:11 +0100

    w

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+var name = prompt ('Enter your name');
+alert('hello, ' + name);
+console.log('Helo, ' + name);
+
+var points = 1;
+
+console.log( 'The current value of points is: ', points ); // 1
+console.log( 'The current value of points is: ', points++ ); // 1
+console.log( 'The current value of points is: ', points ); // 2
